d597e3017bcb2825fc711294b5db482f366626fe
Murilo Baduy <129693902+LeDoque@users.noreply.github.com>
2023-11-08 17:15:16 -0300

    selectionsort e correcoes

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,29 +1,32 @@
-public class BubbleSort {
+import java.sql.SQLOutput;
+
+public class SelectionSort {
     public static void main(String[] args) {
         int[] vetor = new int[10000];
 
-        for (int i = 0; i < vetor.length; i++) {
+        for(int i = 0; i < vetor.length; i++) {
             vetor[i] = (int) (Math.random() * vetor.length);
             System.out.println(vetor[i]);
         }
 
         long startTime = System.currentTimeMillis();
-        int holder;
+        int menor, holder;
         int trocas = 0;
         int iteracoes = 0;
 
-        for (int i = 0; i < vetor.length; i++) {
-            for (int j = i + 1; j < vetor.length; j++) {
+        for(int i = 0; i < vetor.length; i++) {
+            menor = i;
+            for(int j = i + 1; j < vetor.length; j++){
                 iteracoes++;
-                if (vetor[i] > vetor[j]) {
-                    holder = vetor[j];
-                    vetor[j] = vetor[i];
-                    vetor[i] = holder;
-                    trocas++;
+                if (vetor[j] < vetor[menor]){
+                    menor = j;
                 }
             }
+            holder = vetor[menor];
+            vetor[menor] = vetor[i];
+            vetor[i] = holder;
+            trocas++;
         }
-
         long endTime = System.currentTimeMillis();
         long tempoDeExecucao = endTime - startTime;
 
@@ -37,4 +40,3 @@ public class BubbleSort {
         System.out.println("Número de Iterações: " + iteracoes);
     }
 }
-
